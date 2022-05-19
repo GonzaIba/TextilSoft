c0a6a26c4e656d0188b76537be5244596a23282c
Gonzalo_Sarsy <gonzalo.ibanez@sarsy-srl.com>
2022-05-19 10:01:52 -0300

    Se agregan cambios.

---
+++ Aplicacion/Domain/Entities/EmpleadosEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class EmpleadosEntity
+    {
+        public Guid ID_Empleados { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public string Residencia { get; set; }
+        public string Legajo { get; set; }
+        public string DNI { get; set; }
+
+        //public virtual ICollection<PedidosEntity> Pedidos { get; set; }
+    }
+}
